5e15929175ac4921fb5fe4127eadd02d3a5aa848
kunchok tashi <khakontas33@gmail.com>
2026-07-18 10:20:23 -0400

    feat: added app layout

---
+++ src/app/features/booking/booking-flow/booking-flow.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-flow',
+  imports: [],
+  templateUrl: './booking-flow.html',
+  styleUrl: './booking-flow.scss',
+})
+export class BookingFlow {}
